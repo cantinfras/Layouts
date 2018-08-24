541ef1cac48d3815b3704068d8cb8ccaf0d98515
Elton Horacio <cantinfras2@gmail.com>
2018-08-24 17:54:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/elton/layouts/GridLayout.java
@@ -0,0 +1,13 @@
+package com.example.elton.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridlayout);
+    }
+}
